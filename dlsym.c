ce36a60fddb0f312ce875b70bbb1cf61e1aa5715
Frank <frankli0324@hotmail.com>
2021-08-02 14:25:29 +0800

    dlsym gnu hashtable lookup

--- dlsym.c
+++ dlsym.c
@@ -34,6 +34,36 @@ static uint32_t gnu_hash(const char *s0) {
     return h;
 }
 
+static Elf_Sym *gnu_lookup(struct Elf_handle_t *handle, const char *s, uint32_t h) {
+    Elf_Sym *syms = handle->dl_info->symtab;
+    char *strtab = handle->dl_info->strtab;
+    int16_t *gversym = handle->dl_info->gversym;
+    Elf_GHashtab *hashtab = handle->dl_info->ghashtab;
+    const size_t *bloomwords = (const void *)(hashtab + 4);
+    size_t f = bloomwords[(h / (8 * sizeof(size_t))) & (hashtab[2] - 1)];
+    if (!(f & (1ul << h % (8 * sizeof(size_t)))))
+        return 0;
+
+    f >>= (h >> hashtab[3]) % (8 * sizeof f);
+    if (!(f & 1))
+        return 0;
+
+    uint32_t nbuckets = hashtab[0];
+    uint32_t *buckets = hashtab + 4 + hashtab[2] * (sizeof(size_t) / 4);
+    uint32_t i = buckets[h % nbuckets];
+    if (!i)
+        return 0;
+    uint32_t *hashval = buckets + nbuckets + (i - hashtab[1]);
+    for (h |= 1;; i++) {
+        uint32_t h2 = *hashval++;
+        if ((h == (h2 | 1)) && (!gversym || gversym[i] >= 0) && !strcmp(s, strtab + syms[i].st_name))
+            return syms + i;
+        if (h2 & 1)
+            break;
+    }
+    return NULL;
+}
+
 static int t_is_invalid_handle(struct Elf_handle_t *handle) {
     return 0;
 }
@@ -60,8 +90,10 @@ static size_t t_count_sym(struct Elf_handle_t *handle) {
 
 static void *t_find_sym(struct Elf_handle_t *handle, const char *symbol) {
     printf("%d\n", t_count_sym(handle));
-    uint32_t hash = sysv_hash(symbol);
-    return sysv_lookup(handle, symbol, hash);
+    if (handle->dl_info->ghashtab != NULL)
+        return gnu_lookup(handle, symbol, gnu_hash(symbol));
+    else
+        return sysv_lookup(handle, symbol, sysv_hash(symbol));
 }
 
 void *t_dlsym(void *handle, const char *symbol) {
